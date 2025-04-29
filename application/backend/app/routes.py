f65e2043dc974be65ec401f8275b97665ac99fdb
ThomasPerez91 <dialldialldiall@gmail.com>
2025-04-29 21:57:44 +0200

    add: db connect & data_loader

--- application/backend/app/routes.py
+++ application/backend/app/routes.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request  # type: ignore
+from app.services.data_loader import insert_data
 
 bp = Blueprint('routes', __name__)
 
-# Simple route pour tester
+
+@bp.route('/api/insert', methods=['GET'])
+def insert_route():
+    result = insert_data()
+    return jsonify(result)
+
+
 @bp.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "Hello depuis Flask API 🚀"})
 
-# Exemple de POST
+
 @bp.route('/api/data', methods=['POST'])
 def receive_data():
     data = request.json
